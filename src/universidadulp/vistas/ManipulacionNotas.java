0755d6f9bbf2bd2f86bad21abf0be4f092eeb2ce
Florencia L <castillofl1990@gmail.com>
2023-09-15 08:12:22 -0300

    manipulacionNotas1

--- src/universidadulp/vistas/ManipulacionNotas.java
+++ src/universidadulp/vistas/ManipulacionNotas.java
@@ -5,7 +5,11 @@
  */
 package universidadulp.vistas;
 
+import java.util.ArrayList;
+import java.util.List;
 import javax.swing.table.DefaultTableModel;
+import universidadulp.accesoADatos.AlumnoData;
+import universidadulp.accesoADatos.InscripcionData;
 import universidadulp.entidades.*;
 
 /**
@@ -13,11 +17,18 @@ import universidadulp.entidades.*;
  * @author marti
  */
 public class ManipulacionNotas extends javax.swing.JInternalFrame {
-
+    
+    private AlumnoData aluData = new AlumnoData();
+    private InscripcionData inscData = new InscripcionData();
+    private List<Alumno> listaAlumnos = new ArrayList<Alumno>();
+    private List<Materia> listaMaterias= new ArrayList<Materia>();
    
     private DefaultTableModel tNotas = new DefaultTableModel(); 
+    
     public ManipulacionNotas() {
         initComponents();
+        Cabecera();
+        armarComboBox();
     }
 
     /**
@@ -34,8 +45,8 @@ public class ManipulacionNotas extends javax.swing.JInternalFrame {
         jbSalirNotas = new javax.swing.JButton();
         jScrollPane2 = new javax.swing.JScrollPane();
         jtNotas = new javax.swing.JTable();
-        jbGuargarNotas = new javax.swing.JButton();
-        jComboBox1 = new javax.swing.JComboBox<>();
+        jbGuardarNotas = new javax.swing.JButton();
+        jcbAlumnos = new javax.swing.JComboBox<>();
 
         jLabel1.setFont(new java.awt.Font("Arial", 0, 24)); // NOI18N
         jLabel1.setText("CARGAR NOTAS");
@@ -57,7 +68,13 @@ public class ManipulacionNotas extends javax.swing.JInternalFrame {
         ));
         jScrollPane2.setViewportView(jtNotas);
 
-        jbGuargarNotas.setText("Guardar");
+        jbGuardarNotas.setText("Guardar");
+
+        jcbAlumnos.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jcbAlumnosActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -76,10 +93,10 @@ public class ManipulacionNotas extends javax.swing.JInternalFrame {
                             .addGroup(layout.createSequentialGroup()
                                 .addComponent(jLabel2)
                                 .addGap(18, 18, 18)
-                                .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, 327, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                                .addComponent(jcbAlumnos, javax.swing.GroupLayout.PREFERRED_SIZE, 327, javax.swing.GroupLayout.PREFERRED_SIZE))))
                     .addGroup(layout.createSequentialGroup()
                         .addGap(143, 143, 143)
-                        .addComponent(jbGuargarNotas)
+                        .addComponent(jbGuardarNotas)
                         .addGap(75, 75, 75)
                         .addComponent(jbSalirNotas)))
                 .addContainerGap(30, Short.MAX_VALUE))
@@ -92,35 +109,68 @@ public class ManipulacionNotas extends javax.swing.JInternalFrame {
                 .addGap(27, 27, 27)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel2)
-                    .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(jcbAlumnos, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(29, 29, 29)
                 .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 153, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(33, 33, 33)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jbSalirNotas)
-                    .addComponent(jbGuargarNotas))
+                    .addComponent(jbGuardarNotas))
                 .addContainerGap(34, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private void jcbAlumnosActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jcbAlumnosActionPerformed
+        // borrarfilas()
+        borrarFilas();
+        
+        Alumno alumno= (Alumno) jcbAlumnos.getModel().getSelectedItem();
+        listaAlumnos = aluData.listarAlumnos();
+        
+        
+//        for (Alumno alu : listaAlumnos) {
+//            tNotas.addRow(new Object[]{
+//               
+//        };
+//        }
+    }//GEN-LAST:event_jcbAlumnosActionPerformed
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JComboBox<Alumno> jComboBox1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JScrollPane jScrollPane2;
-    private javax.swing.JButton jbGuargarNotas;
+    private javax.swing.JButton jbGuardarNotas;
     private javax.swing.JButton jbSalirNotas;
+    private javax.swing.JComboBox<Alumno> jcbAlumnos;
     private javax.swing.JTable jtNotas;
     // End of variables declaration//GEN-END:variables
 
-private void Cabecera (){
-    tNotas.addColumn("Codigo");
-    tNotas.addColumn("Materia");
-    tNotas.addColumn("Nota");
-    jtNotas.setModel(tNotas);
+    private void Cabecera() {
+        tNotas.addColumn("Codigo");
+        tNotas.addColumn("Materia");
+        tNotas.addColumn("Nota");
+        jtNotas.setModel(tNotas);
+
+    }
     
-}
-}
+    private void armarComboBox(){
+        listaAlumnos=aluData.listarAlumnos();
+        
+        for (int i = 0; i < listaAlumnos.size(); i++) {
+            jcbAlumnos.addItem(listaAlumnos.get(i));
+        }
+            
+        }
+    
+    private void borrarFilas(){
+        int f=jtNotas.getRowCount() -1;
+        
+        for ( ; f >= 0; f--) {
+            tNotas.removeRow(f);
+        }
+    }
+    }
+
